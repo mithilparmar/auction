698a1c0ec2aa3e1949f5ba77577b18fe20a98049
mithilparmar <mithil@aashnik.com>
2021-02-22 01:02:08 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,26 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      window.location.href = "http://www.w3schools.com";
-    } else {
-      // No user is signed in.
-      window.location.href = "../index.html";
-    }
-  });
+var firebaseConfig = {
+    apiKey: "AIzaSyA1qedikZYzWMWH7fFmnNoNSRSGr2CU27g",
+    authDomain: "fifa-auction.firebaseapp.com",
+    databaseURL: "https://fifa-auction.firebaseio.com",
+    projectId: "fifa-auction",
+    storageBucket: "fifa-auction.appspot.com",
+    messagingSenderId: "366285181838",
+    appId: "1:366285181838:web:878b8ed9cd9d735039cadf",
+    measurementId: "G-7EJK3JT4HC"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-
+const auth = firebase.auth();
 
 function login() {
 
-    var user_email = document.getElementById("email").value;
-    var user_password = document.getElementById("password").value;
+    var user_email = document.getElementById("email");
+    var user_password = document.getElementById("password");
 
-    firebase.auth().signInWithEmailAndPassword(user_email, user_password)
-    .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+    const promise = auth.signInWithEmailAndPassword(user_email, user_password);
+    promise.catch(e => alert(e.message));
 
-        window.alert("Error: " + errorMessage);
-    });
+    alert("Signed In " + user_email);
 
 }
